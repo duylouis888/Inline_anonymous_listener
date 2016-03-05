11d61290dd6131cfad9894e4791ee5f186ce3ca4
PhucDuy <duylouis888@gmail.com>
2016-03-05 18:11:35 +0700

    implement function can + chi

--- app/src/main/java/com/example/phucduy/inline_anonymous_listener/MainActivity.java
+++ app/src/main/java/com/example/phucduy/inline_anonymous_listener/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.phucduy.inline_anonymous_listener;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,98 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // inline listener
+        Button btnChuyenDoi = (Button)findViewById(R.id.btnChuyenDoi);
+        btnChuyenDoi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTextDL = (EditText)findViewById(R.id.editTextDuongLich);
+                int duonglich = Integer.parseInt(editTextDL.getText().toString());
+
+                String can = "";
+                String chi = "";
+                switch(duonglich%10){
+                    case 0:
+                        can = "Canh";
+                        break;
+                    case 1:
+                        can = "Tân";
+                        break;
+                    case 2:
+                        can = "Nhâm";
+                        break;
+                    case 3:
+                        can = "Quý";
+                        break;
+                    case 4:
+                        can = "Giáp";
+                        break;
+                    case 5:
+                        can = "Ất";
+                        break;
+                    case 6:
+                        can = "Bính";
+                        break;
+                    case 7:
+                        can = "Đinh";
+                        break;
+                    case 8:
+                        can = "Mậu";
+                        break;
+                    case 9:
+                        can = "Kỷ";
+                        break;
+                    default:break;
+                }
+
+                switch(duonglich%12){
+                    case 0:
+                        chi = "Thân";
+                        break;
+                    case 1:
+                        chi = "Dậu";
+                        break;
+                    case 2:
+                        chi = "Tuất";
+                        break;
+                    case 3:
+                        chi = "Hợi";
+                        break;
+                    case 4:
+                        chi = "Tý";
+                        break;
+                    case 5:
+                        chi = "Sửu";
+                        break;
+                    case 6:
+                        chi = "Dần";
+                        break;
+                    case 7:
+                        chi = "Mẹo";
+                        break;
+                    case 8:
+                        chi = "Thìn";
+                        break;
+                    case 9:
+                        chi = "Tỵ";
+                        break;
+                    case 10:
+                        chi = "Ngọ";
+                        break;
+                    case 11:
+                        chi = "Mùi";
+                        break;
+                    default:break;
+                }
+
+                Log.d("Can chi: ",can + " " + chi);
+                String result = can + " " + chi;
+                TextView txtResult = (TextView)findViewById(R.id.txtVKQ);
+                txtResult.setText(result);
+            }
+        });
+
     }
+
 }
